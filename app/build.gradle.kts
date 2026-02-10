e1b2ddbe7156174c2f1355ac74d36a96297272c8
Kunal <kunal@justbaat.com>
2026-02-10 20:15:33 +0530

    bumped versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.app.bishnoi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "4.1"
+        versionCode = 5
+        versionName = "4.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
     }
